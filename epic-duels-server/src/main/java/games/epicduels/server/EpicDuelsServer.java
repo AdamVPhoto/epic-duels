b4e6bc31ab2128ad690756cb8e328206f4f472e6
AdamVPhoto <avisokey3@gmail.com>
2018-04-04 21:41:11 -0700

    Initial window for server.

---
+++ epic-duels-server/src/main/java/games/epicduels/server/EpicDuelsServer.java
@@ -0,0 +1,25 @@
+package games.epicduels.server;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class EpicDuelsServer extends Application {
+	
+	@Override
+	public void start(Stage stage) throws Exception {
+		
+		Parent root = FXMLLoader.load(getClass().getResource("/fxml/ServerView.fxml"));
+		
+		Scene scene = new Scene(root, 500, 500);
+		
+		stage.setScene(scene);
+		stage.show();
+	}
+
+	public static void main(String[] args) {
+		Application.launch(args);
+	}
+}
